caae43e4ec81cbadbda331a07973f56248ee16bd
houshunde <46257154+houshunde@users.noreply.github.com>
2019-01-14 17:44:36 +0800

    Update depl.sh

--- depl.sh
+++ depl.sh
@@ -4,4 +4,4 @@ export RANCHER_ACCESS_KEY=ACB773E950D2B274CA40
 RANCHER_COMPOSE_FILE=rancher-compose.yml
 DOCKER_COMPOSE_FILE=docker-compose.yml
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:~/.local/bin:./
-rancher-compose  --project-name ops  up  -c -d --force-upgrade --pull
+rancher-compose  --project-name ops  up  -c -d  --pull
